1043aab1785802c49e303ffa8afee40a3cda5c4d
Maurice Royce <mauriceroyce@flames.lan>
2016-10-05 11:50:06 -0700

    finish webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
-var webpack = require('webpack')
-var path = require('path')
+var webpack = require('webpack');
+var path = require('path');
+
 
 var BUILD_DIR = __dirname;
-var APP_DIR = path.resolve(__dirname, 'src/');
+var NODE_MODULES = path.resolve(__dirname, 'node_modules/');
+var SRC_DIR = path.resolve(__dirname, 'src/');
+var STYLES_DIR = path.resolve(__dirname, 'styles/');
+
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+    entry: SRC_DIR + '/index.jsx',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
@@ -14,12 +18,27 @@ var config = {
         loaders: [
             {
                 test: /\.jsx?/,
-                include: APP_DIR,
-                exclude: /node_modules/,
-                loaders: ['babel']
-            }
+                include: [SRC_DIR],
+                exclude: NODE_MODULES,
+                loaders: ['babel'],
+            },
+            {
+                test: /\.less$/,
+                include: [STYLES_DIR, NODE_MODULES],
+                loaders: ['style', 'css', 'less'],
+            },
         ]
-    }
+    },
+    resolve: {
+        moduleDirectories: ['node_modules'],
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            lib: NODE_MODULES,
+            src: SRC_DIR,
+            styles: STYLES_DIR,
+        },
+    },
 };
+
 
 module.exports = config;
