3768da9d3a4b789e04493a61ec39938dc14765c8
Maurice Royce <mauriceroyce@flames.lan>
2016-10-06 14:26:29 -0700

    rename constants/Apis -> constants/Api

---
+++ src/scripts/constants/Api.js
@@ -0,0 +1,9 @@
+export default {
+    url: 'http://127.0.0.1:9000/api/',
+    endpoints: {
+        USERS: 'users/',
+        COMPANIES: 'companies/',
+        EVENTS: 'events/',
+        LISTINGS: 'listings/',
+    }
+}
